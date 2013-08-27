e2948b801ac557f773a2372a4bff1ff0ef292775
Tiberiu Craciun <tibi@happysoft.ro>
2013-08-27 22:42:47 +0300

    Added zero empty space

--- 6.cleanup.sh
+++ 6.cleanup.sh
@@ -3,3 +3,8 @@
 sudo apt-get clean
 sudo gem cleanup
 rm -r "$(gem env gemdir)"/doc/*
+cd /tmp
+dd if=/dev/zero of=zero.small.file bs=1024 count=102400
+car /dev/zero > zero.file
+rm zero.small.file
+rm zero.file
