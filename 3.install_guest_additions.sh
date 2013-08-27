250e36ed2643ec5bd7651fd55b362aaf8b026b45
Tiberiu Craciun <tibi@happysoft.ro>
2013-08-27 23:12:37 +0300

    read input only from tty

--- 3.install_guest_additions.sh
+++ 3.install_guest_additions.sh
@@ -6,7 +6,7 @@ sudo apt-geti -y upgrade
 sudo apt-get -y install linux-headers-$(uname -r) build-essential
 echo "Go to Menu > Devices > Install Guest Additions"
 echo "and press ENTER to continue when you are done"
-read
+read -e -n1 a < /dev/tty
 sudo mount /dev/cdrom /media/cdrom
 sudo sh /media/cdrom/VBoxLinuxAdditions.run
 sudo umount /dev/cdrom
