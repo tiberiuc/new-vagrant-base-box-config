6cb35d1bea4721bf827ed0e491a3e196b7cf0569
Tiberiu Craciun <tibi@happysoft.ro>
2013-08-27 22:03:53 +0300

    Change from wget to curl

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 cd /tmp
 
 curl -o 1.sh https://raw.github.com/tiberiuc/master/1.change_sudoers.sh
-wget -o 2.sh https://raw.github.com/tiberiuc/master/2.change_rights_and_root_passwd.sh
-wget -o 3.sh https://raw.github.com/tiberiuc/master/3.install_guest_additions.sh
-wget -o 4.sh https://raw.github.com/tiberiuc/master/4.set_ssh_access.sh
-wget -o 5.sh https://raw.github.com/tiberiuc/master/5.install_software.sh
-wget -o 6.sh https://raw.github.com/tiberiuc/master/6.cleanup.sh
+curl -o 2.sh https://raw.github.com/tiberiuc/master/2.change_rights_and_root_passwd.sh
+curl -o 3.sh https://raw.github.com/tiberiuc/master/3.install_guest_additions.sh
+curl -o 4.sh https://raw.github.com/tiberiuc/master/4.set_ssh_access.sh
+curl -o 5.sh https://raw.github.com/tiberiuc/master/5.install_software.sh
+curl -o 6.sh https://raw.github.com/tiberiuc/master/6.cleanup.sh
 
 chmod +x *.sh
 
