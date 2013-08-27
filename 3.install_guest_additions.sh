b4f0996277f863bd3c3ba5a380cfb56739295a59
Tiberiu Craciun <tibi@happysoft.ro>
2013-08-27 23:00:40 +0300

    Added -y to upgrade

--- 3.install_guest_additions.sh
+++ 3.install_guest_additions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-geti -y upgrade
 
 sudo apt-get -y install linux-headers-$(uname -r) build-essential
 echo "Go to Menu > Devices > Install Guest Additions"
