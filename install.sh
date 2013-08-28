ea94968d8f65100bcec22aeedc033ca1e2e55570
tiberiuc <tibi@happysoft.ro>
2013-08-28 11:11:21 +0300

    Changed repository name

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 cd /tmp
 
-curl -o 1.sh https://raw.github.com/tiberiuc/new_vagrant_base_box_config/master/1.change_sudoers.sh
-curl -o 2.sh https://raw.github.com/tiberiuc/new_vagrant_base_box_config/master/2.change_rights_and_root_passwd.sh
-curl -o 3.sh https://raw.github.com/tiberiuc/new_vagrant_base_box_config/master/3.install_guest_additions.sh
-curl -o 4.sh https://raw.github.com/tiberiuc/new_vagrant_base_box_config/master/4.set_ssh_access.sh
-curl -o 5.sh https://raw.github.com/tiberiuc/new_vagrant_base_box_config/master/5.install_software.sh
-curl -o 6.sh https://raw.github.com/tiberiuc/new_vagrant_base_box_config/master/6.cleanup.sh
+SCRIPTS_URL="https://raw.github.com/tiberiuc/new-vagrant-base-box-config/master/"
+
+curl -o 1.sh ${SCRIPTS_URL}/1.change_sudoers.sh
+curl -o 2.sh ${SCRIPTS_URL}/2.change_rights_and_root_passwd.sh
+curl -o 3.sh ${SCRIPTS_URL}/3.install_guest_additions.sh
+curl -o 4.sh ${SCRIPTS_URL}/4.set_ssh_access.sh
+curl -o 5.sh ${SCRIPTS_URL}/5.install_software.sh
+curl -o 6.sh ${SCRIPTS_URL}/6.cleanup.sh
 
 chmod +x *.sh
 
